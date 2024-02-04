d816e8a6b5b6015377e48798bcaa9accb74fee8c
mnsh5 <hugo.orlando2050@gmail.com>
2024-02-04 17:31:47 -0300

    :rocket: add strings

--- 01_strings.rb
+++ 01_strings.rb
@@ -4,6 +4,10 @@ last_name = "Prakash"
 puts first_name + "" + last_name
 
 # String interpolation
-first_name = "Subhash "
+first_name = "Subhash"
 last_name = "Prakash"
 puts "My first name is #{first_name} and my last name is #{last_name}"
+
+# Methods, how o find them
+full_name = "Subhash Prakash"
+puts "Subhash Prakash is a #{full_name.class}"
