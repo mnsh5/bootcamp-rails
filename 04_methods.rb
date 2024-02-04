e237f8c00928705cad8a7874983d7e731100ff6a
mnsh5 <hugo.orlando2050@gmail.com>
2024-02-04 18:27:17 -0300

    :rocket: add new features

--- 04_methods.rb
+++ 04_methods.rb
@@ -1,3 +1,5 @@
+# -- Methods --
+
 def gretting_one()
   puts "Hello, World!"
 end
@@ -10,7 +12,12 @@ def grettin_three(first_name, last_name="Prakash")
   puts "Hello #{first_name} #{last_name}"
 end
 
+def suma(a,b)
+  return a + b
+end
 
 gretting_one()
 gretting_two("Subhash Prakash")
 grettin_three("Subhash")
+
+puts suma(1,1)
