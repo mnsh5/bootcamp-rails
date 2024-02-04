20add9209a72f2d5240c0ca02dea5fd227184e68
mnsh5 <hugo.orlando2050@gmail.com>
2024-02-04 17:59:29 -0300

    :rocket: add new features

--- 01_strings.rb
+++ 01_strings.rb
@@ -1,3 +1,5 @@
+# -- Strings --
+
 # String concatenation
 first_name = "Subhash "
 last_name = "Prakash"
@@ -14,7 +16,17 @@ puts "Subhash Prakash is a #{full_name.class}"
 puts "length: #{full_name.length}"
 puts "reverse: #{full_name.reverse}"
 puts "capitalize: #{full_name.capitalize}"
+puts "empty: #{full_name.empty?}"
+puts "nil: #{full_name.nil?}"
 
 num = 55
 num2 = num.to_s # Convert integer to a string
 puts "55 is a #{num2.class}"
+
+# Common Methods
+puts "methods: #{full_name.methods}"
+
+# Variable assignment
+first_name = "Lucciano"
+new_first_name = first_name
+puts first_name
---
+++ 02_getting-input.rb
@@ -0,0 +1,7 @@
+# -- Getting Input from user --
+
+puts "What's your name?"
+user_name = gets.chomp
+
+# Displaying the user's input
+puts "Hello, #{user_name}! Nice to meet you."
