6ddedfc7b0f106acb05f34f5dcd9b2a40726f2bd
mnsh5 <hugo.orlando2050@gmail.com>
2024-02-04 18:08:31 -0300

    :rocket: add new features

---
+++ 04_methods.rb
@@ -0,0 +1,16 @@
+def gretting_one()
+  puts "Hello, World!"
+end
+
+def gretting_two(name)
+  puts "Hey #{name}"
+end
+
+def grettin_three(first_name, last_name="Prakash")
+  puts "Hello #{first_name} #{last_name}"
+end
+
+
+gretting_one()
+gretting_two("Subhash Prakash")
+grettin_three("Subhash")
