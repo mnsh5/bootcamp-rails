dba934bf427ea1ddf3e34e51a546c5e338881e4c
mnsh4 <hugo-orlando.gonzalez@atos.net>
2024-02-04 17:27:00 -0300

    :rocket: add strings

---
+++ 01_strings.rb
@@ -0,0 +1,9 @@
+# String concatenation
+first_name = "Subhash "
+last_name = "Prakash"
+puts first_name + "" + last_name
+
+# String interpolation
+first_name = "Subhash "
+last_name = "Prakash"
+puts "My first name is #{first_name} and my last name is #{last_name}"
