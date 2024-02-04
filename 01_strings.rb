b4aefe8b0e39d40d2a9a75c8c8ecebdc57370511
mnsh5 <hugo.orlando2050@gmail.com>
2024-02-04 17:36:37 -0300

    :rocket: add strings  methods

--- 01_strings.rb
+++ 01_strings.rb
@@ -11,3 +11,10 @@ puts "My first name is #{first_name} and my last name is #{last_name}"
 # Methods, how o find them
 full_name = "Subhash Prakash"
 puts "Subhash Prakash is a #{full_name.class}"
+puts "length: #{full_name.length}"
+puts "reverse: #{full_name.reverse}"
+puts "capitalize: #{full_name.capitalize}"
+
+num = 55
+num2 = num.to_s # Convert integer to a string
+puts "55 is a #{num2.class}"
