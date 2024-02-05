40bd3c739dd27c6a68713a6aa0402d9e4c593d56
mnsh5 <hugo.orlando2050@gmail.com>
2024-02-05 20:43:00 -0300

    :tada: add new features

--- opp/student.rb
+++ opp/student.rb
@@ -10,6 +10,37 @@ class Student
   end
 
   def greeting
-    puts "Hello #{@first_name}"
+    puts "Hello #{@first_name} #{@last_name}"
+  end
+
+  # Getter para first_name
+  def get_first_name
+    @first_name
+  end
+
+  # Setter para first_name
+  def set_first_name(new_first_name)
+    @first_name = new_first_name
+  end
+
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, Email: #{@email}"
   end
 end
+
+# Crear una instancia de la clase Student
+student = Student.new("Anastasiya", "Viktorovna", "voronina", "voronina@gmail.com", "russianpass123")
+
+# Utilizar el método greeting
+student.greeting
+
+# Utilizar getters y setters
+puts "Original First Name: #{student.get_first_name}"
+
+# Cambiar el valor de first_name usando el setter
+# student.set_first_name("Jane")
+
+# Obtener el nuevo valor usando el getter
+puts "Updated First Name: #{student.get_first_name}"
+
+puts student.to_s
